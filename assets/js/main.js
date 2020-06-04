7127c970ca0616ef3cbfe0640aac23aa45ac7f03
Odimaa <odimaa.horsfall@gmail.com>
2020-06-04 13:50:27 +0100

    Added my project

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+
+
+// const selectElement = function (element) {
+//     return document.querySelector(element);
+// };
+
+
+// let menuToggler = selectElement('.menu-toggle');
+// let body = selectElement('body');
+
+
+// menuToggler.addEventListener('click', function () {
+//     body.classList.toggle('open');
+// });
+
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 56)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 56
+    });
+
+})(jQuery); // End of use strict
+
